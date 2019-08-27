b8ceeed5e1b7dd2a0700569e8d6644ec28d6ad55
asreal089 <lexx_89@hotmail.com>
2019-08-27 12:12:59 -0300

    ajustes de no script

--- setup.sh
+++ setup.sh
@@ -7,7 +7,9 @@ function trilha {
     fi
 
     message="[$info] `date '+%d/%m/%Y %H:%M:%S'`, $1"
+    echo "-------------------------------------------------------------------------------------------------------------------------------"
     echo $message
+    echo "-------------------------------------------------------------------------------------------------------------------------------"
 }
 
 
@@ -17,3 +19,36 @@ sudo apt-get update
 
 trilha "instalando curl"
 sudo apt-get install curl -y
+
+trilha "instalando sublime-text"
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+sudo apt-get update
+
+sudo apt-get install sublime-text
+
+trilha "instalando vim"
+
+sudo apt-get install vim -y
+
+trilha "instalando git"
+
+sudo apt-get install git
+
+trilha "instalando mercurial"
+
+sudo apt-get install mercurial
+
+trilha "instalando JDK"
+
+sudo apt-get install openjdk-8-jdk -y
+
+trilha "instalando DBEAVER"
+
+
+
+
+
