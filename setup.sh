79dac58f0d416e8d389ac6516f0fa6f808ceed42
asreal089 <lexx_89@hotmail.com>
2019-08-27 15:02:22 -0300

    ajuste para Ambiente Java da Aml

--- setup.sh
+++ setup.sh
@@ -49,6 +49,23 @@ sudo apt-get install openjdk-8-jdk -y
 trilha "instalando DBEAVER"
 
 
+trilha "baixando arquivos de Ambiente java AML"
+curl http://mailing.amlconsulting.com.br/arquivos/arquivosPaulo.tar.gz --output ambienteJava.tar.gz
 
+trilha "Descompactando ambienteJava"
+tar xvzf ambienteJava.tar.gz -C $PWD
 
+trila "criando Diretorio para instalações de ambienteJava da AML"
+mkdir "JAVA_AML_SETUP"
+
+trilha "Descompactando eclipse, mvn e jdk para diretorio install"
+tar xvzf eclipse-standard-luna-R-linux-gtk-x86_64.tar.gz -C "$HOME/JAVA_AML_SETUP"
+tar xvzf apache-maven-3.3.3-bin.tar.gz -C "$HOME/JAVA_AML_SETUP"
+tar xvzf jdk1.7.tar.gz -C "$HOME/JAVA_AML_SETUP"
+
+trilha "configurando Variaveis de Ambiente do java"
+echo  'export JAVA_HOME=$HOME/JAVA_AML_SETUP/jdk1.7.0_17' >> ~/.bashrc 
+echo  'export M2_HOME=$HOME/JAVA_AML_SETUP/apache-maven-3.3.3' >> ~/.bashrc 
+echo  'export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=1024m"' >> ~/.bashrc 
+echo  'export PATH="$JAVA_HOME/bin:$M2_HOME/bin:$PATH"' >> ~/.bashrc 
 
