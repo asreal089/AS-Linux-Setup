b9de93305b5ad59ee5486cffa6737a51bedd0af2
asreal089 <lexx_89@hotmail.com>
2022-06-18 14:11:13 -0300

    Adicionando lamp stack na instalação de ambiente.

--- setup.sh
+++ setup.sh
@@ -121,3 +121,46 @@ sudo apt-get install code
 trilha "instalando Chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+#Instalação do ambiente lamp.
+
+trilha "instalando php7 e suas libs"
+sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql php-cli php-xdebug
+
+trilha "configurando Xdebug no PHP"
+sudo -H /bin/bash
+cd /etc/apt
+echo '[XDebug]' >> /etc/php/7.2/apache2/php.ini 
+echo 'xdebug.remote_enable = 1' >> /etc/php/7.2/apache2/php.ini 
+echo 'xdebug.remote_autostart = 1' >> /etc/php/7.2/apache2/php.ini
+exit
+
+trilha "Configurando root do mysql-server"
+echo "digite a senha do seu usuario mysql"
+
+read senha
+sudo mysql -e "drop user 'root'@'localhost'; create user 'root'@'localhost' identified by '$senha';grant all privileges on *.* to 'root'@'localhost' with grant option; flush privileges;"
+
+trilha "Desabilitando modulos do mysql-server"
+sudo -H /bin/bash
+sed -i '/^\[mysqld\]/d' my.cnf
+sed -i '/^sql_mode*/d' my.cnf
+echo "[mysqld]" >>my.cnf
+echo 'sql_mode = ""' >>my.cnf
+exit
+
+trilha "configurando php.ini para ambiente"
+sudo -H /bin/bash
+cd /etc/php/7.2/apache2
+sed -i '/^short_open_tag \= Off/c\short_open_tag \= On' php.ini
+sed -i '/^display_errors \= Off/c\display_errors \= On' php.ini
+exit
+
+trilha "configurando apache root directory para diretorio apache_server na sua home"
+mkdir "apache_server"
+printf -v var "%q\n" $HOME
+echo "$var"
+sudo -H /bin/bash
+cd /etc/apache2
+sed -i '/^\<Directory \/var\/www\/\>/c\\<Directory $var\/apache_server\/\>' apache2.conf
+exit
